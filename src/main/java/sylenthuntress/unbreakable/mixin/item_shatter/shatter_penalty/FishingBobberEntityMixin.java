b178db7bad6bddf671e0409fa08b86276e4390e1
SylentHuntress <GaymerGirlEva@gmail.com>
2024-12-14 20:17:27 -0600

    Fishing rods now work as intended when shattered

--- src/main/java/sylenthuntress/unbreakable/mixin/item_shatter/shatter_penalty/FishingBobberEntityMixin.java
+++ src/main/java/sylenthuntress/unbreakable/mixin/item_shatter/shatter_penalty/FishingBobberEntityMixin.java
@@ -4,6 +4,7 @@ import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.projectile.FishingBobberEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.util.math.Vec3d;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -17,20 +18,19 @@ import static sylenthuntress.unbreakable.provider.ModItemTagProvider.SHATTER_BLA
 public abstract class FishingBobberEntityMixin {
     @ModifyVariable(method = "<init>(Lnet/minecraft/entity/player/PlayerEntity;Lnet/minecraft/world/World;II)V", at = @At("STORE"))
     private Vec3d FishingBobberEntity$applyShatterPenalty(Vec3d vec3d, PlayerEntity thrower) {
-        ItemStack stack = thrower.getActiveItem();
+        ItemStack stack = thrower.getMainHandStack();
+        if (!stack.isOf(Items.FISHING_ROD)) stack = thrower.getOffHandStack();
         double penaltyMultiplier = 1;
-        if (stack != null && ItemShatterHelper.isShattered(stack) && !stack.isIn(SHATTER_BLACKLIST) && Unbreakable.CONFIG.shatterPenalties.PROJECTILES()) {
+        if (ItemShatterHelper.isShattered(stack) && !stack.isIn(SHATTER_BLACKLIST) && Unbreakable.CONFIG.shatterPenalties.PROJECTILES())
             penaltyMultiplier = ItemShatterHelper.calculateShatterPenalty(stack);
-        }
         return vec3d.multiply(penaltyMultiplier, penaltyMultiplier, penaltyMultiplier);
     }
 
     @ModifyExpressionValue(method = "use", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;getLuck()F"))
     private float use$applyShatterPenalty(float original, ItemStack stack) {
         double penaltyMultiplier = 1;
-        if (stack != null && ItemShatterHelper.isShattered(stack) && !stack.isIn(SHATTER_BLACKLIST) && Unbreakable.CONFIG.shatterPenalties.ARMOR_TOUGHNESS()) {
+        if (ItemShatterHelper.isShattered(stack) && !stack.isIn(SHATTER_BLACKLIST) && Unbreakable.CONFIG.shatterPenalties.ARMOR_TOUGHNESS())
             penaltyMultiplier = ItemShatterHelper.calculateShatterPenalty(stack);
-        }
         return (int) (original * penaltyMultiplier);
     }
 }
