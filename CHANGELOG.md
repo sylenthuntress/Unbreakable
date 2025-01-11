47091d42774bf47d5da9fb9d31834146d5473aa2
SylentHuntress <GaymerGirlEva@gmail.com>
2025-01-11 10:04:19 -0600

    Bumped version + CHANGELOG.md

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,14 +3,20 @@
 Internal rewrite, new features, and more!
 
 **Features**:
-
 * Players can now repair items without XP using smithing tables
     * Conversely, players can now use the grindstone to repair items with _only_ XP levels
     * These options become less effective on a given item until you repair it using other means
         * Anvils completely reset their effectiveness, but are expensive
 * Added plenty of new config options
 
+**Tweaks**:
+
+* Tweaked tool damage calculations based on block hardness
+
 **Technical**:
+
+* Added a registry for ALL items used as `repairIngredients` in any `RepairableComponent` for the smithing table rework
 * Changed how items are disabled when shattered
 * Fixed calculation errors when displaying shatter level
 * Refactored a LOT of code, and overhauled parts of config
+    * Other mods can add their items to the new tags for extra mod compatibility
--- src/main/java/sylenthuntress/unbreakable/util/DataTagKeys.java
+++ src/main/java/sylenthuntress/unbreakable/util/DataTagKeys.java
@@ -10,7 +10,7 @@ public abstract class DataTagKeys {
     public static final TagKey<Item> SHATTER_BLACKLIST = TagKey.of(RegistryKeys.ITEM, Identifier.of(Unbreakable.MOD_ID, "shatter_blacklist"));
     public static final TagKey<Item> BREAKABLE_ITEMS = TagKey.of(RegistryKeys.ITEM, Identifier.of(Unbreakable.MOD_ID, "retains_breaking"));
     public static final TagKey<Item> PREVENT_USE_WHEN_SHATTERED = TagKey.of(RegistryKeys.ITEM, Identifier.of(Unbreakable.MOD_ID, "prevent_use_when_shattered"));
-    public static final TagKey<Item> GOLDEN_TOOLS = TagKey.of(RegistryKeys.ITEM, Identifier.of(Unbreakable.MOD_ID, "golden_tools"));
+    public static final TagKey<Item> GOLDEN_EQUIPMENT = TagKey.of(RegistryKeys.ITEM, Identifier.of(Unbreakable.MOD_ID, "golden_equipment"));
     public static final TagKey<Enchantment> INCOMPATIBLE_ENCHANTMENTS = TagKey.of(RegistryKeys.ENCHANTMENT, Identifier.of(Unbreakable.MOD_ID, "incompatible_enchantments"));
 
 }
