6c390ccccb310fedbc6bff3251e0788b7f2c65a2
SylentHuntress <GaymerGirlEva@gmail.com>
2025-03-09 17:53:13 -0500

    Add id, name, and block fields to RepairStations

--- src/main/java/sylenthuntress/unbreakable/util/RepairHelper.java
+++ src/main/java/sylenthuntress/unbreakable/util/RepairHelper.java
@@ -1,11 +1,14 @@
 package sylenthuntress.unbreakable.util;
 
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
 import net.minecraft.component.DataComponentTypes;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.registry.entry.RegistryEntryList;
+import net.minecraft.util.Identifier;
 import sylenthuntress.unbreakable.Unbreakable;
 import sylenthuntress.unbreakable.registry.UnbreakableComponents;
 
@@ -26,7 +29,7 @@ public class RepairHelper {
         repairMaterials.add(items);
     }
 
-    public static int calculateRepairFactor(int repairConstant, ItemStack outputStack, ItemStack inputStack, boolean scaledWithShatterLevel, RepairStations repairStation) {
+    public static int calculateRepairFactor(int repairConstant, ItemStack outputStack, ItemStack inputStack, boolean scaledWithShatterLevel, RepairStation repairStation) {
         return Math.min(
                 outputStack.getDamage(),
                 outputStack.getMaxDamage() /
@@ -40,19 +43,19 @@ public class RepairHelper {
         );
     }
 
-    private static int calculateRepairConstant(double repairConstant, ItemStack outputStack, ItemStack inputStack, boolean scaledWithShatterLevel, RepairStations repairStation) {
+    private static int calculateRepairConstant(double repairConstant, ItemStack outputStack, ItemStack inputStack, boolean scaledWithShatterLevel, RepairStation repairStation) {
         boolean shatterScaling = false;
         boolean enchantmentScaling = false;
         boolean degradeRepairFactor = false;
         float costMultiplier = 1;
         switch (repairStation) {
-            case RepairStations.SMITHING_TABLE -> {
+            case RepairStation.SMITHING_TABLE -> {
                 shatterScaling = Unbreakable.CONFIG.smithingRepair.COST.SHATTER_SCALING();
                 enchantmentScaling = Unbreakable.CONFIG.smithingRepair.COST.ENCHANTMENT_SCALING();
                 degradeRepairFactor = Unbreakable.CONFIG.smithingRepair.COST.DEGRADE_REPAIR_FACTOR();
                 costMultiplier = Unbreakable.CONFIG.smithingRepair.COST.MULTIPLIER();
             }
-            case RepairStations.GRINDSTONE -> {
+            case RepairStation.GRINDSTONE -> {
                 shatterScaling = Unbreakable.CONFIG.grindingRepair.COST.SHATTER_SCALING();
                 enchantmentScaling = Unbreakable.CONFIG.grindingRepair.COST.ENCHANTMENT_SCALING();
                 degradeRepairFactor = Unbreakable.CONFIG.grindingRepair.COST.DEGRADE_REPAIR_FACTOR();
@@ -67,14 +70,14 @@ public class RepairHelper {
             for (RegistryEntry<Enchantment> enchantment : outputStack.getEnchantments().getEnchantments())
                 repairConstant += ItemShatterHelper.getEnchantmentLevel(enchantment.getKey().orElseThrow(), outputStack);
         if (degradeRepairFactor)
-            if (repairStation == RepairStations.GRINDSTONE)
-                repairConstant /= 1 + (outputStack.getOrDefault(UnbreakableComponents.GRINDING_DEGRADATION, 0) * 0.1);
-            else repairConstant /= 1 + (outputStack.getOrDefault(UnbreakableComponents.SMITHING_DEGRADATION, 0) * 0.1);
+            if (repairStation == RepairStation.GRINDSTONE)
+                repairConstant /= 1 + (outputStack.getOrDefault(UnbreakableComponents.DEGRADATION_GRINDING, 0) * 0.1);
+            else repairConstant /= 1 + (outputStack.getOrDefault(UnbreakableComponents.DEGRADATION_SMITHING, 0) * 0.1);
         repairConstant *= costMultiplier;
         return (int) Math.round(repairConstant);
     }
 
-    public enum RepairStations {
+    public enum RepairStation {
         SMITHING_TABLE("smithing_table", 0, Blocks.SMITHING_TABLE),
         GRINDSTONE("grindstone", 1, Blocks.GRINDSTONE);
 
@@ -82,13 +85,13 @@ public class RepairHelper {
         private final int id;
         private final List<Block> blocks;
 
-        RepairStations(final String name, final int id, final List<Block> blocks) {
+        RepairStation(final String name, final int id, final List<Block> blocks) {
             this.name = Unbreakable.modIdentifier(name);
             this.id = id;
             this.blocks = blocks;
         }
 
-        RepairStations(final String name, final int id, final Block blocks) {
+        RepairStation(final String name, final int id, final Block blocks) {
             this(name, id, List.of(blocks));
         }
     }
